8b9dc8d6548806f3349e33c88ec6bfc3065bfeab
Snoopsie1 <oliver.rasoli@gmail.com>
2022-02-03 23:46:06 +0100

    found error, localhost cant be found on GA server

--- src/main/java/DataAccess/DBconnector.java
+++ src/main/java/DataAccess/DBconnector.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBconnector {
-    private static String URL = "jdbc:mysql://localhost:3306/startcode_test?serverTimezone=Europe/Copenhagen";
+    private static String URL = "jdbc:mysql://127.0.0.1:3306/startcode_test?serverTimezone=Europe/Copenhagen";
     private static String USER = "dev";
     private static String PW = "ax2";
 
